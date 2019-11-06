766ee1a4612fe7763fdcae14771d4c04724b815c
Nico Einsidler <nicoeinsidler@gmail.com>
2019-11-06 10:14:53 +0100

    added find modules capabilities

--- pigor.py
+++ pigor.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
 import pathlib
+import pkgutil
 
+import plugins
 
+# get all available experiments in plugins
+experiments_list = []
+for importer, modname, ispkg in pkgutil.iter_modules(plugins.__path__):
+    if ispkg:
+        experiments_list.append(modname)
+
+print(experiments_list)
