5c3e7e54b42e44e8c992c72c99fff87982abe997
Nico Einsidler <nicoeinsidler@gmail.com>
2019-11-06 18:12:02 +0100

    added @alter_data

---
+++ pigor/library/decorators.py
@@ -0,0 +1,40 @@
+import pandas
+
+def alter_data(func):
+    """ Decorator to modify data in dataframe. The modified data will then be stored in a new column inside the data frame.
+    
+    :param func: function which will be decorated
+    :type func: callable function
+    :return: extended data frame
+    :rtype: pandas.DataFrame
+    """
+    def wrapper(data: pandas.DataFrame, *args, **kwargs) -> dict:
+        # get newly calculated data
+        new_data = func(data, *args, **kwargs)
+
+        # get number of data points in data frame
+        data_length = len(data.index)
+        # store new data as new column(s)
+        for k, v in new_data.items():
+            # if newly created data is not enough pad it with zeros
+            if data_length > len(v):
+                v.extend([0] * (data_length-len(v)))
+
+            data[k] = v[0:data_length]
+
+        return data
+    return wrapper
+
+# @alter_data
+# def test_alter_data(data: pandas.DataFrame) -> dict:
+#     return {
+#         'third' : [1,3,4,5,4,1,1,1,1,1,1,1,1,1,1],
+#         'fourth': [1,2,3,4,5]
+#     }
+
+# t = {
+#     'first' : [1,3,4,5,3,2,2,3],
+#     'second' : [5,3,3,5,3,9,2,3]
+# }
+# a = pandas.DataFrame.from_dict(t)
+# print(test_alter_data(a))
