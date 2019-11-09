56f6e9d22cf53f1e259e336a64d363d39667852c
Nico Einsidler <nicoeinsidler@gmail.com>
2019-11-09 23:00:58 +0100

    got the imports right :D

--- pigor/library/render.py
+++ pigor/library/render.py
@@ -1,11 +1,10 @@
 import sys
 import pathlib
 
-PATH_TO_PLUGINS = pathlib.Path('../plugins').resolve().absolute()
-print(PATH_TO_PLUGINS)
+PATH_TO_PLUGINS = pathlib.Path('../..').resolve().absolute()
 if str(PATH_TO_PLUGINS) not in sys.path:
     sys.path.insert(0, str(PATH_TO_PLUGINS))
-# sys.path.append(os.path.abspath(os.path.join('..', 'plugins')))
-print(sys.path)
 
-from plugins.polarimeter.functions import *
+
+import pigor.plugins
+
