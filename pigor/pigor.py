fb6b11a358faaf6790cc6c30163ee09ed7ccbff8
Nico Einsidler <nicoeinsidler@gmail.com>
2019-11-10 00:09:05 +0100

    added list of all plugins

--- pigor/pigor.py
+++ pigor/pigor.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-
+import re
 import pathlib
-import pkgutil
 
-import .plugins
+# regex pattern to identify __pychache__
+PYTHON_DIR_PATTERN = re.compile('__[A-Za-z0-9._-]*__')
+# path were all plugins are located
+PATH_TO_PLUGINS = pathlib.Path('./plugins')
 
-# get all available experiments in plugins
-experiments_list = []
-for importer, modname, ispkg in pkgutil.iter_modules(plugins.__path__):
-    if ispkg:
-        experiments_list.append(modname)
+all_plugins = [entry for entry in PATH_TO_PLUGINS.glob('*') if entry.is_dir() and not re.match(PYTHON_DIR_PATTERN, entry.name)]
 
-print(experiments_list)
