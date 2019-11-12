76f155332b5f5d41dc9f2af9400ccbc755c42e30
Nico Einsidler <nicoeinsidler@gmail.com>
2019-11-12 01:20:22 +0100

    started to work on fit

---
+++ pigor/library/fit.py
@@ -0,0 +1,21 @@
+import lmfit
+import pandas
+
+
+def fit(data: pandas.DataFrame, fit_config: dict) -> [lmfit.model.ModelResult]:
+    """ Performs a fit using lmfit.
+    
+    :param data: data to fit
+    :type data: pandas.DataFrame
+    :param fit_config:  describes what columns are used for 
+                        x- and y-axis and how they should be
+                        fitted, for example {'column1':'x',
+                        'column2':lmfit.models.GaussianModel, 
+                        'column3':lmfit.models.LinearModel},
+                        where column1 etc. are the actual
+                        names (or numbers) of the pandas data
+                        frame columns
+    :type fit_config: dict
+    :return: list of finished fits
+    :rtype: [lmfit.model.ModelResult]
+    """
