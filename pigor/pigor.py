1c09665a823478927edd24909d4a5e250b4e8553
Nico Einsidler <nicoeinsidler@gmail.com>
2020-01-11 14:44:39 +0100

    moves is_plugin to library

--- pigor/pigor.py
+++ pigor/pigor.py
@@ -10,6 +10,8 @@ PIGOR_ROOT_PATH = pathlib.Path('../').resolve().absolute()
 if str(PIGOR_ROOT_PATH) not in sys.path:
     sys.path.insert(0, str(PIGOR_ROOT_PATH))
 
+from pigor.library.functions import is_plugin
+
 # regex pattern to identify __pychache__
 PYTHON_DIR_PATTERN = re.compile('__[A-Za-z0-9._-]*__')
 # path were all plugins are located
@@ -19,42 +21,6 @@ all_plugins = [
     entry for entry in PATH_TO_PLUGINS.glob('*')
     if entry.is_dir() and not re.match(PYTHON_DIR_PATTERN, entry.name)
 ]
-
-
-def is_plugin(path: pathlib.Path) -> bool:
-    """ Verifies if path matches all criteria for a plugin:
-
-    1. must be a directory
-    2. directory must contain an __init__.py file
-    3. must have an adapter.py file
-    4 :code:`read(file_path: pathlib.Path) -> pandas.DataFrame` function in adapter.py 
-    
-    :param path: path to the plugin folder
-    :type path: pathlib.Path
-    :return: returns :code:`True` if path points to a valid plugin, otherwise :code:`False`
-    :rtype: bool
-
-    .. todo:: Check if read function takes a pathlib.Path object as first argument and returns a pandas data frame object.
-    """
-    # believe path points to module if not otherwise proven wrong
-    result = True
-
-    # try to import checking criteria 1) & 2)
-    try:
-        plugin = importlib.import_module('pigor.plugins.' + path.name)
-    except ModuleNotFoundError:
-        plugin = None
-        result = False
-
-    # see if adapter.py exists, cirterion 3)
-    if not path.joinpath('adapter.py').exists():
-        result = False
-
-    # check for the existance of read() function
-    if plugin and not 'read' in dir(plugin):
-        result = False
-
-    return result
 
 
 class Measurement(object):
@@ -111,7 +77,8 @@ if __name__ == "__main__":
         '../testfiles/polarimeter/2018-11-22-1125-degree-of-polarisation.dat')
     plugin = 'polarimeter'
     m = Measurement(p, plugin)
-    print(m.data)
+    # print(m.data)
+    # m.decorator_test()
 
     # import fire
     # fire.Fire(Measurement)
